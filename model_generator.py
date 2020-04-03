d2641644d48018a7316ee68dc132255f7e27e9fc
GriskaZ <grigoriozaly@yandex.ru>
2020-04-03 14:01:51 +0600

    модель генератора

---
+++ model_generator.py
@@ -0,0 +1,17 @@
+from tensorflow.keras.models import Model, load_model		
+import numpy as np
+
+#specify the path
+path = ''
+generator = load_model(path +'13500_GENERATOR_weights_and_arch.hdf5')
+
+#specify the images count
+im_count = 1
+
+images = generator.predict(np.random.normal(0, 1, (im_count, 100)))
+
+#visualization
+for img in images:
+  plt.imshow(img.squeeze())
+  plt.show()
+
